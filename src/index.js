3c8919cae383f90238d62a4ffe3092e3edd7e390
ritik <gupta.ritik2002@gmail.com>
2023-03-28 13:59:03 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ const { PORT } = require("./config/serverConfig");
 
 const apiRoutes = require("./routes/index");
 
+// const UserRepository = require("./repository/user-repository");
+
 const app = express();
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api", apiRoutes);
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server Started on PORT:${PORT}`);
+    // const repo = new UserRepository();
+    // const response = await repo.getById(1);
+    // console.log(response);
   });
 };
 
